a3a36339e9443e2e3e85439743ab323cc276e256
thorhallur-iceland <ragnarsson-p@ulster.ac.uk>
2021-05-02 02:11:34 +0000

    Update assignment_script.py

--- assignment/assignment_script.py
+++ assignment/assignment_script.py
@@ -34,8 +34,27 @@ myCRS = ccrs.UTM(29)  # create a Universal Transverse Mercator reference system 
 variable = 'NumberOfFa'
 # set the range for the choropleth values
 vmin, vmax = 0, 1200
+
 # create figure and axes for Matplotlib
 fig, ax = plt.subplots(1, 1, figsize=(10,10), subplot_kw=dict(projection=myCRS))
+
+
+# add a title
+ax.set_title('Total Farms in each District', fontdict={'fontsize': '25', 'fontweight': '3'})
+
+# Create colorbar legend
+sm = plt.cm.ScalarMappable(cmap='Reds', norm=plt.Normalize(vmin=vmin, vmax=vmax))
+
+# empty array for the data range
+sm.set_array([])
+# add the colorbar to the figure
+ax_sm = fig.colorbar(sm, ax=ax)
+ax_sm.set_label('Total Farms', fontdict={'fontsize': '15', 'fontweight': '3'})
+# create map
+farms.plot(column=variable, cmap='Reds', linewidth=0.8, ax=ax, edgecolor='0.8', transform=myCRS)
+
+# save and print figure
+fig.savefig('map.png', dpi=300)
 
 # Create a scatterplot graph of the relationship between Farms and Farmers
 fig2, ax2 = plt.subplots(figsize=(10, 6))
@@ -46,7 +65,6 @@ plt.ylabel('Farms')
 plt.title('Relations between Number of Farmers and Farms')
 # save and print figure
 fig2.savefig('scatterplot.png', dpi=300)
-
 
 # Create a pie chart, dividing each farm animal into a wedge represented with total number of each animal and percentage
 fig3, ax3 = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
@@ -70,21 +88,3 @@ plt.setp(autotexts, size=8, weight="bold")
 ax3.set_title("Division of Animals in Northern Ireland")
 # save and print figure
 fig3.savefig('piechart.png')
-
-# add a title
-ax.set_title('Total Farms in each District', fontdict={'fontsize': '25', 'fontweight': '3'})
-
-# Create colorbar legend
-sm = plt.cm.ScalarMappable(cmap='Reds', norm=plt.Normalize(vmin=vmin, vmax=vmax))
-
-# empty array for the data range
-sm.set_array([])
-# add the colorbar to the figure
-ax_sm = fig.colorbar(sm, ax=ax)
-ax_sm.set_label('Total Farms', fontdict={'fontsize': '15', 'fontweight': '3'})
-# create map
-farms.plot(column=variable, cmap='Reds', linewidth=0.8, ax=ax, edgecolor='0.8', transform=myCRS)
-
-# save and print figure
-fig.savefig('map.png', dpi=300)
-
